41176c96b5f69ec6507813efc2747e8927faf558
kushal <kushaldeveloper@gmail.com>
2014-03-07 22:56:16 -0600

    get name and email into code behind so we can get plumbing later

--- ModelWeb/Default.aspx
+++ ModelWeb/Default.aspx
@@ -2,6 +2,7 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-
-
+    <asp:TextBox id="txtModelName" runat="server"></asp:TextBox>
+    <asp:TextBox id="txtModelMail" runat="server" AutoCompleteType="Email"></asp:TextBox>
+    <asp:Button id="btnSubmit" runat="server" Text="Submit" OnClick="submit_Click" />
 </asp:Content>
--- ModelWeb/Default.aspx.cs
+++ ModelWeb/Default.aspx.cs
@@ -9,9 +9,18 @@ namespace ModelWeb
 {
     public partial class _Default : Page
     {
+        string myModelName;
+        string myModelEmail;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            myModelName = txtModelName.Text;
+            myModelEmail = txtModelMail.Text;
         }
     }
 }
